e2feb727c527ad86e3657a6e6464cb8d21a64af4
mattkumar <mattkumar@gmail.com>
2025-10-24 11:20:48 -0400

    zzz

--- app.R
+++ app.R
@@ -5,9 +5,9 @@ library(officer)
 set.seed(123)
 patient_data <- data.frame(
   id = paste0("P", sprintf("%02d", 1:10)),
-  sex = sample(c("Male", "Female"), 10, replace = TRUE),
-  age_group = sample(c("18-30", "31-50", "51-70", "70+"), 10, replace = TRUE),
-  biomarker = sample(c("Positive", "Negative", "Borderline"), 10, replace = TRUE),
+  weight = runif(10, min = 110, max = 210),
+  height = runif(10, min = 150, max = 184),
+  age = rnorm(10, mean = 30, sd = 10),
   stringsAsFactors = FALSE
 )
 
@@ -16,15 +16,15 @@ ui <- fluidPage(
   sidebarLayout(
     sidebarPanel(
       selectInput("x_var", "X-axis Variable:", 
-                  choices = c("sex", "age_group", "biomarker"), 
-                  selected = "sex"),
-      selectInput("fill_var", "Fill Variable:", 
-                  choices = c("sex", "age_group", "biomarker"), 
-                  selected = "biomarker"),
+                  choices = c("weight", "height", "age"), 
+                  selected = "weight"),
+      selectInput("y_var", "Y-axis Variable:", 
+                  choices = c("weight", "height", "age"), 
+                  selected = "height"),
       downloadButton("download_plot", "Download Plot")
     ),
     mainPanel(
-      plotOutput("stacked_plot", height = "500px")
+      plotOutput("scatter_plot", height = "500px")
     )
   )
 )
@@ -32,88 +32,31 @@ ui <- fluidPage(
 server <- function(input, output, session) {
   
   create_plot <- reactive({
-    if (input$x_var == input$fill_var) return(NULL)
-    
-    # Create count data
-    plot_data <- aggregate(id ~ get(input$x_var) + get(input$fill_var), 
-                           data = patient_data, 
-                           FUN = length)
-    names(plot_data) <- c("x_var", "fill_var", "n")
-    
-    # Get unique categories
-    x_cats <- unique(plot_data$x_var)
-    fill_cats <- unique(plot_data$fill_var)
-    
-    # Create color palette
-    colors <- rainbow(length(fill_cats), alpha = 0.8)
-    
-    # Create the plot using base R graphics
     function() {
-      # Set up the plot area
-      par(mar = c(5, 4, 4, 2) + 0.1)
-      
-      # Calculate bar positions
-      n_x <- length(x_cats)
-      n_fill <- length(fill_cats)
-      bar_width <- 0.8 / n_fill
-      
-      # Find max count for y-axis
-      max_count <- max(plot_data$n)
-      
-      # Create empty plot
-      plot(1, type = "n", xlim = c(0.5, n_x + 0.5), ylim = c(0, max_count * 1.1),
-           xlab = tools::toTitleCase(gsub("_", " ", input$x_var)),
-           ylab = "Count",
-           main = paste("Bar Plot:", input$x_var, "by", input$fill_var),
-           xaxt = "n", cex.main = 1.2)
-      
-      # Add x-axis labels
-      axis(1, at = 1:n_x, labels = x_cats)
-      
-      # Draw bars
-      for (i in 1:nrow(plot_data)) {
-        x_pos <- which(x_cats == plot_data$x_var[i])
-        fill_pos <- which(fill_cats == plot_data$fill_var[i])
-        
-        bar_x <- x_pos - 0.4 + (fill_pos - 1) * bar_width + bar_width/2
-        
-        rect(bar_x - bar_width/2, 0, bar_x + bar_width/2, plot_data$n[i],
-             col = colors[fill_pos], border = "white", lwd = 1)
-        
-        # Add text labels
-        text(bar_x, plot_data$n[i] + max_count * 0.02, 
-             plot_data$n[i], pos = 3, font = 2, cex = 0.8)
-      }
-      
-      # Add legend
-      legend("topright", legend = fill_cats, fill = colors,
-             title = tools::toTitleCase(gsub("_", " ", input$fill_var)),
-             bg = "white", box.lwd = 1)
+      plot(patient_data[[input$x_var]], patient_data[[input$y_var]],
+           xlab = input$x_var,
+           ylab = input$y_var,
+           main = paste("Plot:", input$x_var, "vs", input$y_var),
+           pch = 16, col = "blue", cex = 1.2)
     }
   })
   
-  output$stacked_plot <- renderPlot({
+  output$scatter_plot <- renderPlot({
     plot_func <- create_plot()
-    if (is.null(plot_func)) {
-      plot(1, type = "n", xlab = "", ylab = "", main = "", axes = FALSE)
-      text(1, 1, "Please select different variables", cex = 1.5, col = "gray50")
-    } else {
-      plot_func()
-    }
+    plot_func()
   })
   
   output$download_plot <- downloadHandler(
     filename = function() paste0("result_with_qr_", Sys.Date(), ".pptx"),
     content = function(file) {
       plot_func <- create_plot()
-      if (is.null(plot_func)) stop("No plot to save")
       
       png_f <- tempfile(fileext = ".png")
       png(png_f, width = 1200, height = 800, res = 300, bg = "white")
       plot_func()
       dev.off()
       
-      qr_data <- paste("User matt\nDate", Sys.Date(), "\nFilters", input$x_var, input$fill_var)
+      qr_data <- paste("User matt\nDate", Sys.Date(), "\nFilters", input$x_var, input$y_var)
       qr_png_f <- tempfile(fileext = ".png")
       png(qr_png_f, width = 200, height = 200)
       plot(qrcode::qr_code(qr_data))
@@ -131,3 +74,4 @@ server <- function(input, output, session) {
 }
 
 shinyApp(ui = ui, server = server)
+
